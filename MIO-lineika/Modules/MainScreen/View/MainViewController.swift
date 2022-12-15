c386c90faf9125b23c4b66a6dc07601bb77a27a4
Alexey Zubkov <alexey200223@gmail.com>
2022-12-15 23:43:03 +0300

    Initial commit

---
+++ MIO-lineika/Modules/MainScreen/View/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MIO-lineika
+//
+//  Created by Alexey Zubkov on 15.12.2022.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
---
+++ MIO-lineika/Modules/MainScreen/ViewModel/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  MIO-lineika
+//
+//  Created by Alexey Zubkov on 15.12.2022.
+//
+
+final class MainViewModel {}
